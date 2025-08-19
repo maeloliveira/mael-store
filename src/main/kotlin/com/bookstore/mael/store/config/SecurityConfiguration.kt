327457c359a83da6b02dbaf4b7498d27acadba28
Ismael Oliveira <mael.jozi@gmail.com>
2025-08-19 01:11:24 -0300

    Fix security v2

--- src/main/kotlin/com/bookstore/mael/store/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/bookstore/mael/store/config/SecurityConfiguration.kt
@@ -2,13 +2,56 @@ package com.bookstore.mael.store.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfiguration {
+    private val PUBLIC_MATCHERS = arrayOf<String>(
+        "/customer"
+    )
 
     @Bean
-    fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
-        return BCryptPasswordEncoder()
+    fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+
+        http
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers(HttpMethod.POST, *PUBLIC_MATCHERS).permitAll()
+                    .anyRequest().authenticated()
+            }
+            .formLogin(withDefaults())
+            .httpBasic(withDefaults())
+
+        return http.build()
     }
+
+    @Bean
+    fun userDetailsService(passwordEncoder: BCryptPasswordEncoder): UserDetailsService {
+        val user: UserDetails = User.builder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+        val admin = User.builder()
+            .username("admin")
+            .password("password")
+            .roles("ADMIN", "USER")
+            .build();
+        return InMemoryUserDetailsManager(user, admin);
+    }
+
 }
